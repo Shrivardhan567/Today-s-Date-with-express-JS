c8dd464481a888a6715d6202ec3491b087e29c06
shrivardhan <shrivardhanmallekedi@gmail.com>
2023-09-18 13:59:00 +0000

    Today's Date solution

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+module.exports = app;
+
+app.get("/", (request, response) => {
+  let date = new Date();
+  response.send(
+    `${date.getDate()}-${date.getMonth() + 1}-${date.getFullYear()}`
+  );
+});
+
+app.listen(3000);
